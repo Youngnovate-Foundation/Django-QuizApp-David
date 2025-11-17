736d2e01db1d548f14259c2f33b12e2e3b0773d3
Living Epistle <datiemokeseku16@gmail.com>
2025-11-17 15:04:04 +0000

    Create login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.views import View
+from django.contrib import messages
 
 # Create your views here.
+class Index_View(View):
+    def get(self, request):
+        login_form = AuthenticationForm()
+        return render(request, 'users/index.html', {'login_form': login_form})
+    
+    def post(self, request):
+        login_form = AuthenticationForm(data=request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Successfully Logged-In")
+                return redirect('')
+            else:
+                messages.error(request, f'Login Failed!!')
